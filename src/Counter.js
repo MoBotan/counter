13ee344cf0d2cedf779c6d10e49f7cb5956e1f14
MoBotan <38035587+MoBotan@users.noreply.github.com>
2022-02-06 21:10:42 +1100

    first commit

---
+++ src/Counter.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { ThemeContext } from "./App";
+
+export default class Component extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: props.initialCount,
+    };
+  }
+
+  render() {
+    return (
+      <ThemeContext.Consumer>
+        {(style) => (
+          <>
+            <button style={style} onClick={() => this.changeCount(-1)}>
+              -
+            </button>
+            <span>{this.state.count}</span>
+            <button style={style} onClick={() => this.changeCount(1)}>
+              +
+            </button>
+          </>
+        )}
+      </ThemeContext.Consumer>
+    );
+  }
+
+  changeCount(amount) {
+    this.setState((prevState) => {
+      return { count: prevState.count + amount };
+    });
+  }
+}
---
+++ src/CounterHooks.js
@@ -0,0 +1,24 @@
+import React, { useState, useContext } from "react";
+import Button from "./Button";
+
+export const CountContext = React.createContext();
+
+export default function CounterHooks({ initialCount }) {
+  const [count, setCount] = useState(initialCount);
+  return (
+    <CountContext.Provider value={setCount}>
+      <>
+        {/* <button style={style} onClick={() => setCount((prevCount) => prevCount - 1)}>
+          -
+        </button>
+        <span>{count}</span>
+        <button style={style} onClick={() => setCount((prevCount) => prevCount + 1)}>
+        -
+      </button> */}
+        <Button changeCount={{ amount: -1, text: "-" }} />
+        <span>{count}</span>
+        <Button changeCount={{ amount: 1, text: "+" }} />
+      </>
+    </CountContext.Provider>
+  );
+}
